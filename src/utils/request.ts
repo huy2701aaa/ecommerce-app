a658f59b19c3a333025c9754befbc32cf98a4563
huydao <daohuy2701@gmail.com>
2024-10-17 22:34:38 +0700

    fix: config vercel

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://ecommerce-api-ckuj.onrender.com/api/v1";
+const BASE_URL = "http://localhost:5000/api/v1";
 
 export const request = axios.create({
   baseURL: BASE_URL,
